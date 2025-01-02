c8bc847ae73b02753116cc0e714c1b918e850834
Farooque Sajjad <22sw040@students.muet.edu.pk>
2025-01-02 06:45:46 -0700

    Create Variables_and_DataTypes.java

---
+++ 01Variables_and_Data_Types/Variables_and_DataTypes.java
@@ -0,0 +1,30 @@
+
+
+public class Variables_and_DataTypes {
+    public static void main(String[] args) {
+
+
+//  A data type in programming defines the type of data a variable can hold. It specifies:
+//   The size of memory required for storing the data.
+//   The range of values the variable can store.
+//  The operations that can be performed on that data.
+        // like :- int, float, boolean, char, string --> primitive data types
+
+
+        // declaring and initializing variables
+        int age = 22;
+        float salary = 4000000.44f;
+        char gender = 'M';
+        boolean isGraduate = false;
+        String name = "Farooque Sajjad";
+
+        System.out.println();
+        System.out.println("Name : " + name);
+        System.out.println("Age : " + age);
+        System.out.println("Salary : " + salary);
+        System.out.println("Gender : " + gender);
+        System.out.println("Graduated? : " + isGraduate);
+    }
+
+
+}
